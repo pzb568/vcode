1961ed7be4a2e181a1ed3d40583025de6a41cf1d
pzb568 <pengzb886@gmail.com>
2019-08-04 15:33:58 +0800

    V语言变量声明

---
+++ 02V语言变量.v
@@ -0,0 +1,11 @@
+fn main() {
+	// 变量声明
+	// := 明和初始化
+	// = 分配
+	name := 'Bob'
+	age := 20
+	large_number := i64(9999999999)
+					 	println(name)
+						println(age)
+						println(large_number)
+					}
